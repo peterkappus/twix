412a022b19abd128d7efd7a8a440c0f79eecccd5
Peter Kappus <peterk@peterkappus.com>
2012-09-13 01:26:22 +0100

    clean up sample app

--- sample_app.rb
+++ sample_app.rb
@@ -1,12 +1,10 @@
 require './twix.rb'
 
-#puts "#{url}\n#{tweet.created_at.strftime("%Y-%m-%d")}   @#{tweet.from_user} #{tweet.text}\n"
-
 #get a pic
 pic = Twix.get_pic("awesome")
 
 #print the text of the tweet
 puts pic[:tweet].text
 
-#download it (-s silent -L follow redirect)
+#download the image file (-s silent -L follow redirect)
 `curl -Ls #{pic[:url]} > #{Time.now.to_i.to_s}.jpg`
